cc903a426abf4535fb7070140fd9dd5c79ebfd30
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-03-25 10:19:44 +0700

    deklarasi menu

--- PraktikumFunction/PraktikumFunction.cpp
+++ PraktikumFunction/PraktikumFunction.cpp
@@ -26,11 +26,18 @@ int pembagian(int bil1, int bil2) {
     return bil1 / bil2;
 }
 
-
-
-
-
-
 int main() {
+    int pilihan;
 
+    do
+    {
+        system("cls");
+        cout << "========Menu=========" << endl;
+        cout << "1.penjumlahan" << endl;
+        cout << "2.pengurangan" << endl;
+        cout << "3.perkalian" << endl;
+        cout << "4.penmbagian" << endl;
+        cout << "5.keluar" << endl;
+        cout << "Masukkan menu yang ingin di pilih (1/2/3/4/5)";
+    } while (true);
 }
